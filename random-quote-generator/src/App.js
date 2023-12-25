4dee3b5c88157dd2047d0663d9ee691a18cd040e
fiyin Oladele <fiyin_oladele@yahoo.com>
2023-12-25 12:36:38 -0800

    Adding another project - randomly generated quotes

---
+++ random-quote-generator/src/App.js
@@ -0,0 +1,39 @@
+//import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    // fetch a random quote when the component mounts
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      setQuote(data.content);
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Random Quote Generator</h1>
+      <h3>The Project Plan</h3>
+      <ul>
+        <li>Fetch and display a random quote from the Quotable API.</li>
+        <li>Include a button to fetch a new quote.</li>
+        <li>Explore handling asynchronous data fetching in React.</li>
+      </ul>
+      <p></p>
+      <blockquote>{quote}</blockquote>
+      <button onClick={fetchQuote}>Get New Quote</button>
+    </div>
+  );
+}
+
+export default App;
